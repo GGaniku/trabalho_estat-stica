bbd62ee038bce6299660091662121cfedfa72f45
GGaniku <gabriel_ganiku@hotmail.com>
2022-07-22 18:16:23 -0300

    algumas atualizações

--- estatistica.py
+++ estatistica.py
@@ -1,8 +1,12 @@
 from main import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 data = []
 
-a = int(input('Você quer fazer os cálculos a partir de um \033[32mnovo banco de dados\033[m [1] ou a partir do \033[32mBD já inserido no sistema\033[m [2]? '))
+# Escolher entre inserir um novo banco de dados ou utilizar o que coletei para o trabalho
+
+'''a = int(input('Você quer fazer os cálculos a partir de um \033[32mnovo banco de dados\033[m [1] ou a partir do \033[32mBD já inserido no sistema\033[m [2]? '))
 if a == 1:
     print('NOVO BANCO DE DADOS:')
     while True:
@@ -11,7 +15,8 @@ if a == 1:
             data.append(insert_data)
         elif insert_data == 0:
             print('Inserção de dados realizada com sucesso.')
-            break
+            break'''
+a = 2
 if a == 2:
     print('USAR O BANCO DE DADOS NO SISTEMA:')
     data = [18, 19, 24, 19, 18, 18, 18, 20, 22, 18, 18, 18, 18, 20, 21, 18, 18, 18, 19, 17, 18, 17, 19, 19, 19, 17, 17, 20, 19, 34, 18, 19, 19, 19, 18]
@@ -19,8 +24,32 @@ if a == 2:
 print(f'''Dados brutos: {data}
 Quantidade de dados: {len(data)}''')
 
+# Ordenar as observações da variável escolhida:
+
 data = sorted(data)
 print(f'Os dados ordenados em ordem crescente é: {data}')
 
+# Determinar a média entre as observações:
+
 media_dados = media(data)
 print(f'a média é: {media_dados}')
+
+## Fazer a Distribuição de frequência!:
+
+#   Tirar as observações repetidas da variável:
+
+#frequencias = no_rep(data)
+#print(f'''Frequências: {frequencias}
+#Número de frequência: ''')
+freq = frequency(data)
+print(freq)
+#   
+
+# Calcular a 
+
+'''y = enumerate(len(data))
+plt.scatter(data,y)
+plt.show()'''
+
+'''print(list(numpy.digitilize()))'''
+'''print(list.index(data))'''
--- main.py
+++ main.py
@@ -2,3 +2,35 @@ def media(data_list):
     n = len(data_list)
     sumlist = sum(data_list)
     return sumlist / n
+
+def no_rep(myList):
+    resultantList = []
+    for element in myList:
+        if element not in resultantList:
+            resultantList.append(element)
+    return resultantList
+
+def frequency(myList):
+    freq = []
+    for element in myList:
+        if element in freq:
+            freq[element] += 1
+        '''else:
+            freq[element] = 1'''
+    return freq
+
+'''def frequency(my_list): 
+    import numpy as np
+    freq = {}
+    # Função "unique" retorna um array com todos elementos existentes
+    # uma única vez (sem repetição).
+    for item in np.unique(np.array(my_list)):
+        # Pesquisa na lista a frequencia de cada elemento
+        # A função "where" retorna um array com os índices onde ocorre o item.
+        # A ideia é só usar o tamanho desse array que sabemos a frequência.
+         freq[item] = np.where(np.array(my_list)==item)[0].shape[0]
+
+    for key, value in freq.items():
+        print (f"{key} : {value}")
+
+    return frequency(my_list)'''
